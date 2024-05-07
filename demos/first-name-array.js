a274ddeaaa780f1f215da3daac4bcb8dc4c0de22
CodeXand3rr <jballesteross235@gmail.com>
2024-05-07 13:12:02 -0400

    saves and notes

---
+++ demos/first-name-array.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let firstNames = ["Nedret", "Chris", "Nathaniel", "Yusuf"];
+console.log(firstNames[1]); //Chris
+
+// 0 is the first number instead of the number 1 
+// the I in a exsercise means index
